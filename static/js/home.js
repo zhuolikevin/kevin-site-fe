2a836d0e4dd5bbcc82b66b2ac4d87cee03f2e0f9
lizhuo <lizhuogo@gmail.com>
2016-09-24 09:47:08 +0800

    [ADD] Add pacline

--- static/js/home.js
+++ static/js/home.js
@@ -1,22 +1,45 @@
-function drawBackground() {
-    var canvas = document.getElementById('background-canvas');
+var TOTAL_DOTS = 50;
+var CANVAS_WIDTH = 800;
+var CANVAS_HEIGHT = 20;
+var PACMAN_COLOR = '#fffb00';
+
+var DOTS_INTERVAL = CANVAS_WIDTH / TOTAL_DOTS;
+var PACMAN_RADIUS = DOTS_INTERVAL / 2;
+
+function drawPacLine(percentage) {
+    var canvas = document.getElementById('pac-line');
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
 
-        ctx.fillRect(0, 0, 150, 150);   // Draw a rectangle with default settings
-        ctx.save();                  // Save the default state
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-        ctx.fillStyle = '#09F';      // Make changes to the settings
-        ctx.fillRect(15, 15, 120, 120); // Draw a rectangle with new settings
-        //
-        ctx.save();                  // Save the current state
-        ctx.fillStyle = '#FFF';      // Make changes to the settings
-        ctx.globalAlpha = 0.5;
-        ctx.fillRect(30, 30, 90, 90);   // Draw a rectangle with new settings
-        ctx.restore();               // Restore previous state
-        ctx.fillRect(45, 45, 60, 60);   // Draw a rectangle with restored settings
-        ctx.restore();               // Restore original state
-        ctx.fillRect(60, 60, 30, 30);
+        // Pac-dots
+        ctx.save();
+        ctx.translate(DOTS_INTERVAL / 2, CANVAS_HEIGHT / 2);
+        for (var i = Math.floor(percentage / 2); i < TOTAL_DOTS; i++) {
+            ctx.save();
+            ctx.translate(i * DOTS_INTERVAL, 0);
+            ctx.beginPath();
+            ctx.arc(0, 0, 2, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fill();
+            ctx.restore();
+        }
+        ctx.restore();
+
+        // Pacman
+        ctx.save();
+        ctx.translate((percentage - 1) * DOTS_INTERVAL / 2, CANVAS_HEIGHT / 2);
+        ctx.fillStyle = PACMAN_COLOR;
+        ctx.beginPath();
+        if (percentage % 2 === 0) {
+            ctx.arc(0, 0, PACMAN_RADIUS, 0, Math.PI * 2);
+        } else {
+            ctx.arc(0, 0, PACMAN_RADIUS, Math.PI / 7, -Math.PI / 7);
+        }
+        ctx.lineTo(0, 0);
+        ctx.fill();
+        ctx.restore();
     }
 }
 
@@ -48,6 +71,10 @@ $(document)
         });
         bar.animate(1.0);
 
-        drawBackground();
+        var i = 0;
+        setInterval(function() {
+            drawPacLine(i);
+            i = (i + 1) % 100;
+        }, 200);
     }
 );
