f093f9f5ba83f2a364dada8fb0c4923f629da973
lizhuo <lizhuogo@gmail.com>
2016-12-06 18:34:36 +0800

    ADD: Hover link to pacman github

--- static/js/home.js
+++ static/js/home.js
@@ -1,40 +1,48 @@
-$(document)
-  .ready(function() {
-    $('#home-content .name').animate({
-      top: 0,
-      opacity: 1
-    }, 500);
-    $('#home-content .slogan').delay(300).animate({
-      top: 0,
-      opacity: 1
-    }, 500);
-    $('#home-content .position').delay(600).animate({
-      top: 0,
-      opacity: 1
-    }, 500);
-    $('#home-content .contact').delay(900).animate({
-      top: 0,
-      opacity: 1
-    }, 500);
+$(document).ready(function() {
+  $('#home-content .name').animate({
+    top: 0,
+    opacity: 1
+  }, 500);
+  $('#home-content .slogan').delay(300).animate({
+    top: 0,
+    opacity: 1
+  }, 500);
+  $('#home-content .position').delay(600).animate({
+    top: 0,
+    opacity: 1
+  }, 500);
+  $('#home-content .contact').delay(900).animate({
+    top: 0,
+    opacity: 1
+  }, 500);
 
-    var bar = new ProgressBar.Circle('#profile-circle', {
-      strokeWidth: 2,
-      easing: 'easeInOut',
-      duration: 1000,
-      color: '#5f91a0',
-      svgStyle: null
+  var bar = new ProgressBar.Circle('#profile-circle', {
+    strokeWidth: 2,
+    easing: 'easeInOut',
+    duration: 1000,
+    color: '#5f91a0',
+    svgStyle: null
+  });
+  bar.animate(1.0);
+
+  var i = 0,
+    pacProgress = new PacProgress('#pac-progressbar', {
+      width: 800,
+      height: 20,
+      totalDots: 30
     });
-    bar.animate(1.0);
+  setInterval(function() {
+    pacProgress.draw(i);
+    i = (i + 1) % 100;
+  }, 500);
+});
 
-    var i = 0,
-      pacProgress = new PacProgress('#pac-progressbar', {
-        width: 800,
-        height: 20,
-        totalDots: 30
-      });
-    setInterval(function() {
-      pacProgress.draw(i);
-      i = (i + 1) % 100;
-    }, 500);
-  }
-);
+$('#project-canvas')
+  .mouseover(function() {
+    $(this).css('border-color', '#fafafa');
+    $('#project-canvas .redirect-tag').css({ opacity: 1 });
+  })
+  .mouseleave(function() {
+    $(this).css('border-color', 'transparent');
+    $('#project-canvas .redirect-tag').css({ opacity: 0 });
+  });
