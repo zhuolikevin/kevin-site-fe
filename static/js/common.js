aa1c097bf0cdd8259d4bd0915775d7b137489f38
lizhuo <lizhuogo@gmail.com>
2017-01-28 21:19:39 -0600

    MOD: add construction notes

--- static/js/common.js
+++ static/js/common.js
@@ -2,7 +2,7 @@ $(window).scroll(function() {
   if ($(window).scrollTop() > 0) {
     $('#nav-bar').css({
       backgroundColor: '#f3f3f3',
-      boxShadow: '0px 1px 5px rgba(0,0,0,0.2)'
+      boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.2)'
     });
   } else if ($(window).scrollTop() === 0) {
     $('#nav-bar').css({
