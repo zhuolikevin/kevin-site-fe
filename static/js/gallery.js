d9b8fa68fe0380ff4bad8db92e1dd6ad8eb9d67b
lizhuo <lizhuogo@gmail.com>
2017-06-12 10:43:26 -0500

    vue

---
+++ static/js/gallery.js
@@ -0,0 +1,23 @@
+// For Debug Mode
+Vue.config.devtools = true;
+
+// eslint-disable-next-line no-unused-vars
+var app = new Vue({
+  el: '#app',
+  data: function() {
+    return {
+      photoUrlList: [
+        'https://c1.staticflickr.com/5/4248/34358100580_d598607fe5_h.jpg',
+        'https://c1.staticflickr.com/5/4244/33933768993_e888a1bff1_h.jpg',
+        'https://c1.staticflickr.com/5/4157/33933770833_e283199b01_h.jpg',
+        'https://c1.staticflickr.com/5/4223/34358105820_7ac1763607_h.jpg',
+        'https://c1.staticflickr.com/5/4203/34702455176_019e79dd7f_h.jpg',
+        'https://c1.staticflickr.com/5/4227/34611129531_11f73c3e5a_h.jpg',
+        'https://c1.staticflickr.com/5/4180/34702466046_3e1b2b9c74_h.jpg',
+        'https://c1.staticflickr.com/5/4190/34611133841_b1e6c3f99e_h.jpg',
+        'https://c1.staticflickr.com/5/4273/34611135001_8afdbf5572_h.jpg',
+        'https://c1.staticflickr.com/5/4194/34702477786_1f8e7ba635_h.jpg'
+      ]
+    };
+  }
+});
