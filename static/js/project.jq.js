2646f56068a3ff79c75ce6b0d7c39dd8c94c3e1b
lizhuo <lizhuogo@gmail.com>
2017-06-23 10:56:14 -0500

    zlank project animation

--- static/js/project.jq.js
+++ static/js/project.jq.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-  $('.slider').slider();
+  // $('.slider').slider();
+  $('#zlank-section .content .title').animate({
+    opacity: 1
+  }, 500);
+  $('#zlank-section .content .abstract').delay(300).animate({
+    top: 0,
+    opacity: 1
+  }, 500);
+  $('#zlank-section .content .details .l1').delay(600).animate({
+    left: 0,
+    opacity: 1
+  }, 500);
+  $('#zlank-section .content .details .l2').delay(900).animate({
+    left: 0,
+    opacity: 1
+  }, 500);
+  $('#zlank-section .content .details .l3').delay(1200).animate({
+    left: 0,
+    opacity: 1
+  }, 500);
+  $('#zlank-section .content .learn-more').delay(1500).animate({
+    opacity: 1
+  }, 500);
 });
