0ad8394ee1ba8ef2b0e285d02cd8885dea8ab276
lizhuo <lizhuogo@gmail.com>
2017-06-26 10:35:16 -0500

    project pacprogress

--- static/js/project.jq.js
+++ static/js/project.jq.js
@@ -24,7 +24,8 @@ $(document).ready(function() {
   }, 500);
 });
 $(window).scroll(function() {
-  if ($(window).scrollTop() >= $('#upintheair-section').position().top * 0.5) {
+  if ($(window).scrollTop() > $('#upintheair-section').position().top - 0.5 * $(window).height() &&
+  $(window).scrollTop() <= $('#pacprogress-section').position().top - 0.5 * $(window).height()) {
     $('#upintheair-section .content .title').animate({
       opacity: 1
     }, 500);
@@ -49,6 +50,31 @@ $(window).scroll(function() {
       opacity: 1
     }, 500);
     $('#upintheair-section .content .learn-more').delay(1800).animate({
+      opacity: 1
+    }, 500);
+  }
+  if ($(window).scrollTop() > $('#pacprogress-section').position().top - 0.5 * $(window).height() &&
+  $(window).scrollTop() <= $('#parking-section').position().top - 0.5 * $(window).height()) {
+    $('#pacprogress-section .content .title').animate({
+      opacity: 1
+    }, 500);
+    $('#pacprogress-section .content .abstract').delay(300).animate({
+      top: 0,
+      opacity: 1
+    }, 500);
+    $('#pacprogress-section .content .details .l1').delay(600).animate({
+      left: 0,
+      opacity: 1
+    }, 500);
+    $('#pacprogress-section .content .details .l2').delay(900).animate({
+      left: 0,
+      opacity: 1
+    }, 500);
+    $('#pacprogress-section .content .details .l3').delay(1200).animate({
+      left: 0,
+      opacity: 1
+    }, 500);
+    $('#pacprogress-section .content .learn-more').delay(1500).animate({
       opacity: 1
     }, 500);
   }
