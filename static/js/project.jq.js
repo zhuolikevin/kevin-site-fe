8375a5d2799c306aeba84d1ffaaf91f671bdb5d0
lizhuo <lizhuogo@gmail.com>
2017-06-26 11:21:33 -0500

    parking section

--- static/js/project.jq.js
+++ static/js/project.jq.js
@@ -78,4 +78,53 @@ $(window).scroll(function() {
       opacity: 1
     }, 500);
   }
+  if ($(window).scrollTop() > $('#parking-section').position().top - 0.5 * $(window).height() &&
+  $(window).scrollTop() <= $('#weather-section').position().top - 0.5 * $(window).height()) {
+    $('#parking-section .content .title').animate({
+      opacity: 1
+    }, 500);
+    $('#parking-section .content .abstract').delay(300).animate({
+      top: 0,
+      opacity: 1
+    }, 500);
+    $('#parking-section .content .details .l1').delay(600).animate({
+      left: 0,
+      opacity: 1
+    }, 500);
+    $('#parking-section .content .details .l2').delay(900).animate({
+      left: 0,
+      opacity: 1
+    }, 500);
+    $('#parking-section .content .details .l3').delay(1200).animate({
+      left: 0,
+      opacity: 1
+    }, 500);
+    $('#parking-section .content .learn-more').delay(1500).animate({
+      opacity: 1
+    }, 500);
+  }
+  // if ($(window).scrollTop() > $('#weather-section').position().top - 0.5 * $(window).height()) {
+  //   $('#weather-section .content .title').animate({
+  //     opacity: 1
+  //   }, 500);
+  //   $('#weather-section .content .abstract').delay(300).animate({
+  //     top: 0,
+  //     opacity: 1
+  //   }, 500);
+  //   $('#weather-section .content .details .l1').delay(600).animate({
+  //     left: 0,
+  //     opacity: 1
+  //   }, 500);
+  //   $('#weather-section .content .details .l2').delay(900).animate({
+  //     left: 0,
+  //     opacity: 1
+  //   }, 500);
+  //   $('#weather-section .content .details .l3').delay(1200).animate({
+  //     left: 0,
+  //     opacity: 1
+  //   }, 500);
+  //   $('#weather-section .content .learn-more').delay(1500).animate({
+  //     opacity: 1
+  //   }, 500);
+  // }
 });
